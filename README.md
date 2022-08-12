93d97a398096704795eb4dee51670f598c132d43
Francesco Oliva <olibartfast@gmail.com>
2022-08-13 00:05:35 +0200

    yolov7 work in progress

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 ## C++ Triton client to infer YoloV4/V5 and YoloV7 models 
 
+## Work in progress
 
 ## Build client libraries
 https://github.com/triton-inference-server/client/tree/r22.06
@@ -19,10 +20,27 @@ https://github.com/triton-inference-server/client/tree/r22.06
 * cmake -DCMAKE_BUILD_TYPE=Release .. 
 * make
 
+## YoloV7
+* go to repo
+* export to onnx the model python export.py --weights <yolo_version>.pt --grid  --simplify --topk-all 100 --iou-thres 0.65 --conf-thres 0.35 --img-size 640 640 --max-wh 640
+
+### YoloV7 TensorRT export
+* To Do
+
+
+## YoloV5
+* To Do
+
+## YoloV4
+* To Do
+
+### Notes
+When you export your model to tensorrt your version MUST match the one supported by your Triton version
+
+
 ## How to run
 * ./yolo-triton-cpp-client  --video=/path/to/video/videoname.format
 * ./yolo-triton-cpp-client  --help for all available parameters
 
 ### Realtime inference test on video
-* Inference test ran from VS Code: https://youtu.be/IUdbplJlspg
-* other video inference test: https://youtu.be/VsENXGMNlhA
+
---
+++ Triton.hpp
@@ -0,0 +1,185 @@
+#pragma once
+#include "common.hpp"
+#include "Yolo.hpp"
+
+namespace Triton{
+
+    enum ProtocolType { HTTP = 0, GRPC = 1 };
+
+
+    struct TritonModelInfo {
+        std::string output_name_;
+        std::vector<std::string> output_names_;
+        std::string input_name_;
+        std::string input_datatype_;
+        // The shape of the input
+        int input_c_;
+        int input_h_;
+        int input_w_;
+        // The format of the input
+        std::string input_format_;
+        int type1_;
+        int type3_;
+        int max_batch_size_;
+
+        std::vector<int64_t> shape_;
+
+    };
+
+
+    TritonModelInfo setModel(const int batch_size){
+        TritonModelInfo yoloModelInfo;
+        yoloModelInfo.output_names_ = std::vector<std::string>{"output"};
+        yoloModelInfo.input_name_ = "image";
+        yoloModelInfo.input_datatype_ = std::string("FP32");
+        // The shape of the input
+        yoloModelInfo.input_c_ = Yolo::INPUT_C;
+        yoloModelInfo.input_w_ = Yolo::INPUT_W;
+        yoloModelInfo.input_h_ = Yolo::INPUT_H;
+        // The format of the input
+        yoloModelInfo.input_format_ = "FORMAT_NCHW";
+        yoloModelInfo.type1_ = CV_32FC1;
+        yoloModelInfo.type3_ = CV_32FC3;
+        yoloModelInfo.max_batch_size_ = 32;
+        yoloModelInfo.shape_.push_back(batch_size);
+        yoloModelInfo.shape_.push_back(yoloModelInfo.input_c_);
+        yoloModelInfo.shape_.push_back(yoloModelInfo.input_h_);
+        yoloModelInfo.shape_.push_back(yoloModelInfo.input_w_);
+        return yoloModelInfo;
+    }
+
+    union TritonClient
+    {
+        TritonClient()
+        {
+            new (&httpClient) std::unique_ptr<tc::InferenceServerHttpClient>{};
+        }
+        ~TritonClient() {}
+
+        std::unique_ptr<tc::InferenceServerHttpClient> httpClient;
+        std::unique_ptr<tc::InferenceServerGrpcClient> grpcClient;
+    };
+
+
+
+    std::vector<uint8_t> Preprocess(
+        const cv::Mat& img, const std::string& format, int img_type1, int img_type3,
+        size_t img_channels, const cv::Size& img_size)
+    {
+        // Image channels are in BGR order. Currently model configuration
+        // data doesn't provide any information as to the expected channel
+        // orderings (like RGB, BGR). We are going to assume that RGB is the
+        // most likely ordering and so change the channels to that ordering.
+        std::vector<uint8_t> input_data;
+        cv::Mat sample;
+        cv::cvtColor(img, sample,  cv::COLOR_BGR2RGB);
+        if (sample.size() != img_size)
+        {
+            cv::resize(sample, sample, img_size);
+        }
+
+        sample.convertTo(
+            sample, (img_channels == 3) ? img_type3 : img_type1);
+
+        const int INPUT_W = Yolo::INPUT_W;
+        const int INPUT_H = Yolo::INPUT_H;
+        int w, h, x, y;
+        float r_w = INPUT_W / (sample.cols * 1.0);
+        float r_h = INPUT_H / (sample.rows * 1.0);
+        if (r_h > r_w)
+        {
+            w = INPUT_W;
+            h = r_w * sample.rows;
+            x = 0;
+            y = (INPUT_H - h) / 2;
+        }
+        else
+        {
+            w = r_h * sample.cols;
+            h = INPUT_H;
+            x = (INPUT_W - w) / 2;
+            y = 0;
+        }
+        cv::Mat re(h, w, CV_8UC3);
+        cv::resize(sample, re, re.size(), 0, 0, cv::INTER_CUBIC);
+        cv::Mat out(INPUT_H, INPUT_W, CV_8UC3, cv::Scalar(128, 128, 128));
+        re.copyTo(out(cv::Rect(x, y, re.cols, re.rows)));
+        out.convertTo(sample, CV_32FC3, 1.f / 255.f);
+
+
+        // Allocate a buffer to hold all image elements.
+        size_t img_byte_size = sample.total() * sample.elemSize();
+        size_t pos = 0;
+        input_data.resize(img_byte_size);
+
+        // (format.compare("FORMAT_NCHW") == 0)
+        //
+        // For CHW formats must split out each channel from the matrix and
+        // order them as BBBB...GGGG...RRRR. To do this split the channels
+        // of the image directly into 'input_data'. The BGR channels are
+        // backed by the 'input_data' vector so that ends up with CHW
+        // order of the data.
+        std::vector<cv::Mat> input_bgr_channels;
+        for (size_t i = 0; i < img_channels; ++i)
+        {
+            input_bgr_channels.emplace_back(
+                img_size.height, img_size.width, img_type1, &(input_data[pos]));
+            pos += input_bgr_channels.back().total() *
+                input_bgr_channels.back().elemSize();
+        }
+
+        cv::split(sample, input_bgr_channels);
+
+        if (pos != img_byte_size)
+        {
+            std::cerr << "unexpected total size of channels " << pos << ", expecting "
+                << img_byte_size << std::endl;
+            exit(1);
+        }
+
+        return input_data;
+    }
+
+
+    auto
+    Postprocess(
+        tc::InferResult* result,
+        const size_t batch_size,
+        const std::vector<std::string>& output_names, const bool batching)
+    {
+        if (!result->RequestStatus().IsOk())
+        {
+            std::cerr << "inference  failed with error: " << result->RequestStatus()
+                << std::endl;
+            exit(1);
+        }
+
+        std::vector<float> detections;
+        std::vector<int64_t> shape;
+
+
+        float* outputData;
+        size_t outputByteSize;
+        for (auto outputName : output_names)
+        {
+            if (outputName == "output")
+            { 
+                result->RawData(
+                    outputName, (const uint8_t**)&outputData, &outputByteSize);
+
+                tc::Error err = result->Shape(outputName, &shape);
+                detections = std::vector<float>(outputByteSize / sizeof(float));
+                std::memcpy(detections.data(), outputData, outputByteSize);
+                if (!err.IsOk())
+                {
+                    std::cerr << "unable to get data for " << outputName << std::endl;
+                    exit(1);
+                }
+            }
+
+        }
+
+        return make_tuple(detections, shape);
+    }
+
+}
