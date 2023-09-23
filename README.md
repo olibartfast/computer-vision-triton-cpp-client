2bbc4eded42efd2dba16a34f9fbf703c52453a27
Francesco Oliva <olibartfast@gmail.com>
2023-09-23 21:42:31 +0200

    Added inference also from single image rather then only video input

--- README.md
+++ README.md
@@ -1,68 +1,85 @@
-## C++ Triton client to infer object detection  models
-* Infer object detection models in a similar way to repo [object-detection-inference](https://github.com/olibartfast/object-detection-inference) but using exclusively Nvidia Triton Server to manage multiple framework backends. 
-* Currently implemented inference for YoloV5/V6/V7/V8, Yolo-Nas. 
-## Build client libraries
-https://github.com/triton-inference-server/client/tree/r22.08
+# C++ Triton Client for Object Detection Models
 
+This C++ application allows you to perform object detection inference using Nvidia Triton Server to manage multiple framework backends. It currently supports object detection models such as YoloV5, YoloV6, YoloV7, YoloV8, and Yolo-Nas.
+
+## Build Client Libraries
+
+To build the client libraries, refer to the Triton Inference Server client libraries located [here](https://github.com/triton-inference-server/client/tree/r22.08).
 
 ## Dependencies
-* Nvidia Triton Inference Server container pulled from NGC (docker pull nvcr.io/nvidia/tritonserver:22.07-py3)
-* Triton client libraries (Tested Release 22.08)
-* Protobuf, Grpc++(versions according to the ones used within Triton server project. I used libraries built inside Triton Client third party folder)
-* rapidjson (on ubuntu sudo apt install rapidjson-dev)
-* libcurl
-* Opencv4(Tested 4.5.4)
 
+Ensure you have the following dependencies installed:
 
+- Nvidia Triton Inference Server container pulled from NGC (docker pull nvcr.io/nvidia/tritonserver:22.07-py3)
+- Triton client libraries (Tested Release 22.08)
+- Protobuf, Grpc++ (versions compatible with Triton Server)
+- RapidJSON (on Ubuntu: sudo apt install rapidjson-dev)
+- libcurl
+- OpenCV 4 (Tested version: 4.5.4)
 
-## Build and compile
-* Set environment variables TritonClientThirdParty_DIR(path/to/client/build/third-party) and TritonClientBuild_DIR(path/to/client/build/install)
-* mkdir build 
-* cd build 
-* cmake -DCMAKE_BUILD_TYPE=Release (or optional -DSHOW_FRAME -DWRITE_FRAME if you want show or write processed frame after inference) .. 
-* make
+## Build and Compile
 
-## YoloV5 export
-* Run from [yolov5 repo](https://github.com/ultralytics/yolov5/issues/251) export script(always onnx case example):  ```python export.py  --weights <yolov5_version>.pt  --include onnx```
+Follow these steps to build and compile the application:
 
-## YoloV6 export
-Weights to export in ONNX format or download from [yolov6 repo](https://github.com/meituan/YOLOv6/tree/main/deploy/ONNX). Posteprocessing code is identical to yolov5-v7.
+1. Set environment variables `TritonClientThirdParty_DIR` (path/to/client/build/third-party) and `TritonClientBuild_DIR` (path/to/client/build/install).
+2. Create a build directory: `mkdir build`
+3. Navigate to the build directory: `cd build`
+4. Run CMake to configure the build:
 
-## YoloV7 export
-* Run from [yolov7 repo](https://github.com/WongKinYiu/yolov7#export) export script(e.g. to onnx) : ```python export.py --weights <yolov7_version>.pt --grid  --simplify --topk-all 100 --iou-thres 0.65 --conf-thres 0.35 --img-size 640 640 --max-wh 640``` (Don't use end-to-end parameter)
+   ```shell
+   cmake -DCMAKE_BUILD_TYPE=Release ..
+   ```
+   
+   Optional flags:
+   - `-DSHOW_FRAME`: Enable to show processed frames after inference.
+   - `-DWRITE_FRAME`: Enable to write processed frames after inference.
+   
+5. Build the application: `make`
 
-## YoloV8 export 
-* Weights to export in ONNX format, [same way as yolov5](https://github.com/ultralytics/ultralytics/tree/main/examples/YOLOv8-CPP-Inference).
+## YoloV5 Export
 
-## Yolo-Nas export 
-* Weights can be export in ONNX format like in [YoloNAS Quickstart](https://github.com/Deci-AI/super-gradients/blob/master/documentation/source/YoloNASQuickstart.md#export-to-onnx).  
-I export the model specifying input and output layers name, for example here below in the case of yolo_nas_s version:
-```
-from super_gradients.training import models
+To export YoloV5 models in ONNX format, you can run the export script from the [yolov5 repository](https://github.com/ultralytics/yolov5/issues/251).
 
-net = models.get("yolo_nas_s", pretrained_weights="coco")
-models.convert_to_onnx(model=net, input_shape=(3,640,640), out_path="yolo_nas_s.onnx", torch_onnx_export_kwargs={"input_names": ['input'], "output_names": ['output0', 'output1']})
-```
+## YoloV6 Export
 
+Export YoloV6 models in ONNX format from the [YoloV6 repository](https://github.com/meituan/YOLOv6/tree/main/deploy/ONNX). Post-processing code for YoloV6 is identical to YoloV5.
+
+## YoloV7 Export
+
+To export YoloV7 models, run the export script from the [YoloV7 repository](https://github.com/WongKinYiu/yolov7#export). Make sure to specify necessary export parameters, such as input and output sizes.
+
+## YoloV8 Export
+
+YoloV8 models can be exported in ONNX format similarly to YoloV5. You can refer to the [Ultralytics YoloV8-CPP-Inference example](https://github.com/ultralytics/ultralytics/tree/main/examples/YOLOv8-CPP-Inference).
+
+## Yolo-Nas Export
+
+To export Yolo-Nas models in ONNX format, follow the instructions in the [YoloNAS Quickstart](https://github.com/Deci-AI/super-gradients/blob/master/documentation/source/YoloNASQuickstart.md#export-to-onnx).
 
 ## Notes
-*  If you trying to export the model to TensorRT or OpenVino, your installed version of previous libraries MUST match the ones supported in Triton release you are using, check [server releases here](https://github.com/triton-inference-server/server/releases) 
 
-### Deploy to Triton
-Set a model repository folder following this [schema](https://github.com/triton-inference-server/server/blob/main/docs/model_repository.md), usually config.pbtxt file is optional except in case of use of OpenVino backend:
+- Ensure that the versions of libraries used for exporting models match the versions supported in the Triton release you are using. Check [Triton Server releases here](https://github.com/triton-inference-server/server/releases).
+
+## Deploy to Triton
+
+To deploy the models to Triton, set up a model repository folder following the [Triton Model Repository schema](https://github.com/triton-inference-server/server/blob/main/docs/model_repository.md). Usually, the `config.pbtxt` file is optional unless you use the OpenVino backend.
+
+Example repository structure:
+
 ```
-<model_repository> 
+<model_repository>
     -> 
-        <model_name> 
+        <model_name>
             -> 
                 [config.pbtx]
                 <model_version>
                     ->
-                         <model_binary>
+                        <model_binary>
 ```
 
-then [run](https://github.com/triton-inference-server/server/blob/main/docs/quickstart.md) the server
-```
+Then, run the Triton server:
+
+```shell
 #!/bin/bash
 $ docker run --gpus=1 --rm \
 -p8000:8000 -p8001:8001 -p8002:8002 \
@@ -70,30 +87,64 @@ $ docker run --gpus=1 --rm \
 nvcr.io/nvidia/tritonserver:<xx.yy>-py3 tritonserver \
 --model-repository=/models
 ```
-If you plan to run on CPU omit --gpus parameter
-* For more info and examples check [triton inference-server tutorials](https://github.com/triton-inference-server/tutorials)
 
-## How to run
+If you plan to run on CPU, omit the `--gpus` parameter.
+
+For more information and examples, refer to the [Triton Inference Server tutorials](https://github.com/triton-inference-server/tutorials).
+
+## How to Run
+
+By using the `--source` parameter with the path to either a video or an image, you can perform object detection on your chosen input type. Follow these instructions:
+
+
+### Performing Inference on a Video
+
+To perform inference on a video, use the `--source` parameter with the path to the video file. The application will process each frame of the video and perform object detection. Here's the command:
+
+```shell
+./object-detection-triton-cpp-client --source=/path/to/video/videoname.format --model_type=<yolo_version> --model=<model_name_folder_on_triton> --labelsFile=/path/to/labels/coco.names --protocol=<http or grpc> --serverAddress=<triton-ip> --port=<8000 for http, 8001 for grpc>
 ```
- ./yolo-triton-cpp-client  --video=/path/to/video/videoname.format  --model_type=<yolo_version> --model=<model_name_folder_on_triton> --labelsFile=/path/to/labels/coco.names
---protocol=<http or grpc> --serverAddress=<triton-ip> --port<8000 for http, 8001 for grpc>
+
+Replace the following placeholders:
+- `/path/to/video/videoname.format`: The path to your video file.
+- `<yolo_version>`: The YOLO version you want to use (e.g., yolov5, yolov6, yolov7, yolov8).
+- `<model_name_folder_on_triton>`: The name of the model folder in the Triton server where your chosen YOLO model is deployed.
+- `/path/to/labels/coco.names`: The path to the file containing label names (e.g., COCO labels).
+- `<http or grpc>`: Choose either `http` or `grpc` as the protocol based on your Triton server setup.
+- `<triton-ip>`: The IP address of your Triton server.
+- `<8000 for http, 8001 for grpc>`: The port number, which is usually `8000` for HTTP or `8001` for gRPC, depending on your Triton server configuration.
+
+### Performing Inference on an Image
+
+To perform inference on an image, use the `--source` parameter with the path to the image file. The application will process the image and perform object detection. Here's the command:
+
+```shell
+./object-detection-triton-cpp-client --source=/path/to/image/image.png --model_type=<yolo_version> --model=<model_name_folder_on_triton> --labelsFile=/path/to/labels/coco.names --protocol=<http or grpc> --serverAddress=<triton-ip> --port=<8000 for http, 8001 for grpc>
 ```
-```
- ./yolo-triton-cpp-client  --help for all available parameters
-```
 
-### Realtime inference test on video
-Testing yolov7-tiny exported to onnx  
-https://youtu.be/lke5TcbP2a0
+Replace the following placeholders:
+- `/path/to/image/image.png`: The path to your image file.
+- `<yolo_version>`: The YOLO version you want to use (e.g., yolov5, yolov6, yolov7, yolov8).
+- `<model_name_folder_on_triton>`: The name of the model folder in the Triton server where your chosen YOLO model is deployed.
+- `/path/to/labels/coco.names`: The path to the file containing label names (e.g., COCO labels).
+- `<http or grpc>`: Choose either `http` or `grpc` as the protocol based on your Triton server setup.
+- `<triton-ip>`: The IP address of your Triton server.
+- `<8000 for http, 8001 for grpc>`: The port number, which is usually `8000` for HTTP or `8001` for gRPC, depending on your Triton server configuration.
 
+Use `./object-detection-triton-cpp-client --help` to view all available parameters.
 
-### References
-* https://github.com/triton-inference-server/client/blob/r21.08/src/c%2B%2B/examples/image_client.cc
-* https://github.com/itsnine/yolov5-onnxruntime
-* https://github.com/wang-xinyu/tensorrtx
+## Realtime Inference Test on Video
 
+Watch a test of YoloV7-tiny exported to ONNX [here](https://youtu.be/lke5TcbP2a0).
 
-### TO DO
-* Manage models with dynamic axis(i.e. input layer -1, -1, -1, -1)
-* Nms on gpu
-* Run the app in a docker container using an image containing all the dependencies
+## References
+
+- [Triton Inference Server Client Example](https://github.com/triton-inference-server/client/blob/r21.08/src/c%2B%2B/examples/image_client.cc)
+- [YoloV5 ONNX Runtime](https://github.com/itsnine/yolov5-onnxruntime)
+- [TensorRTX](https://github.com/wang-xinyu/tensorrtx)
+
+## TO DO
+
+- Manage models with dynamic axis (i.e., input layer -1, -1, -1, -1)
+- NMS (Non-Maximum Suppression) on GPU
+- Dockerize the application with an image containing all dependencies
--- client.cpp
+++ client.cpp
@@ -18,6 +18,70 @@ std::unique_ptr<DetectorInterface> createDetectorInstance(const std::string& mod
     }
 }
 
+std::vector<Yolo::Detection> processSource(const cv::Mat& source, 
+    const std::unique_ptr<DetectorInterface>& detector, 
+    tc::InferOptions& options, 
+    Triton::TritonClient& tritonClient, 
+    const Triton::TritonModelInfo& detectorModelInfo, 
+    Triton::ProtocolType protocol, size_t batch_size)
+{
+    tc::Error err;
+    std::vector<uint8_t> input_data = detector->preprocess(source, detectorModelInfo.input_format_, detectorModelInfo.type1_, detectorModelInfo.type3_,
+        detectorModelInfo.input_c_, cv::Size(detectorModelInfo.input_w_, detectorModelInfo.input_h_));
+
+    std::vector<tc::InferInput*> inputs = { nullptr };
+    std::vector<const tc::InferRequestedOutput*> outputs = Triton::createInferRequestedOutput(detectorModelInfo.output_names_);        
+
+    if (inputs[0] != nullptr) {
+        err = inputs[0]->Reset();
+        if (!err.IsOk()) {
+            std::cerr << "failed resetting input: " << err << std::endl;
+            exit(1);
+        }
+    }
+    else {
+        err = tc::InferInput::Create(
+            &inputs[0], detectorModelInfo.input_name_, detectorModelInfo.shape_, detectorModelInfo.input_datatype_);
+        if (!err.IsOk()) {
+            std::cerr << "unable to get input: " << err << std::endl;
+            exit(1);
+        }
+    }
+
+    err = inputs[0]->AppendRaw(input_data);
+    if (!err.IsOk()) {
+        std::cerr << "failed setting input: " << err << std::endl;
+        exit(1);
+    }
+
+    tc::InferResult* result;
+    std::unique_ptr<tc::InferResult> result_ptr;
+    if (protocol == Triton::ProtocolType::HTTP) {
+        err = tritonClient.httpClient->Infer(
+            &result, options, inputs, outputs);
+    }
+    else {
+        err = tritonClient.grpcClient->Infer(
+            &result, options, inputs, outputs);
+    }
+    if (!err.IsOk()) {
+        std::cerr << "failed sending synchronous infer request: " << err
+            << std::endl;
+        exit(1);
+    }
+
+    auto [infer_results, infer_shapes] = Triton::getInferResults(result, batch_size, detectorModelInfo.output_names_, detectorModelInfo.max_batch_size_ != 0);
+    result_ptr.reset(result);
+
+
+    std::vector<Yolo::Detection> detections = detector->postprocess(cv::Size(source.cols, source.rows),
+        infer_results, infer_shapes);
+
+    return detections;
+}
+
+
+
 // Function to parse input size string and validate the format
 std::pair<int, int> parseInputSize(const std::string& input_size_str) {
     std::istringstream iss(input_size_str);
@@ -42,13 +106,14 @@ static const std::string keys =
     "{ help h   | | Print help message. }"
     "{ model_type t | yolov7 | yolo version used i.e yolov5, yolov6, yolov7, yolov8}"
     "{ model m | yolov7-tiny_onnx | model name of folder in triton }"
-    "{ video v | video.mp4 | video name}"
+    "{ source s | data/dog.jpg | path to video or image}"
     "{ serverAddress  ip  | localhost | server address ip, default localhost}"
     "{ port  p  | 8001 | Port number(Grpc 8001, Http 8000)}"
     "{ verbose vb | false | Verbose mode, true or false}"
     "{ protocol pt | grpc | Protocol type, grpc or http}"
     "{ labelsFile l | ../coco.names | path to coco labels names}"
     "{ batch b | 1 | Batch size}";
+
 int main(int argc, const char* argv[])
 {
     cv::CommandLineParser parser(argc, argv, keys);
@@ -61,7 +126,7 @@ int main(int argc, const char* argv[])
     std::string serverAddress = parser.get<std::string>("serverAddress");
     std::string port = parser.get<std::string>("port");
     bool verbose = parser.get<bool>("verbose");
-    std::string videoName = parser.get<std::string>("video");
+    std::string sourceName = parser.get<std::string>("source");  // Changed from 'video' to 'source'
     Triton::ProtocolType protocol = parser.get<std::string>("protocol") == "grpc" ? Triton::ProtocolType::GRPC : Triton::ProtocolType::HTTP;
     const size_t batch_size = parser.get<size_t>("batch");
 
@@ -74,112 +139,84 @@ int main(int argc, const char* argv[])
     std::cout << "Chosen Parameters:" << std::endl;
     std::cout << "model_type (t): " << parser.get<std::string>("model_type") << std::endl;
     std::cout << "model (m): " << parser.get<std::string>("model") << std::endl;
-    std::cout << "video (v): " << parser.get<std::string>("video") << std::endl;
-    std::cout << "serverAddress (s): " << parser.get<std::string>("serverAddress") << std::endl;
+    std::cout << "source (s): " << parser.get<std::string>("source") << std::endl;  // Changed from 'video' to 'source'
+    std::cout << "serverAddress (ip): " << parser.get<std::string>("serverAddress") << std::endl;
     std::cout << "verbose (vb): " << parser.get<bool>("verbose") << std::endl;
-    std::cout << "protocol (p): " << parser.get<std::string>("protocol") << std::endl;
+    std::cout << "protocol (pt): " << parser.get<std::string>("protocol") << std::endl;
     std::cout << "labelsFile (l): " << parser.get<std::string>("labelsFile") << std::endl;
     std::cout << "batch (b): " << parser.get<size_t>("batch") << std::endl;
-
 
     // Create Triton client
     Triton::TritonClient tritonClient;
     Triton::createTritonClient(tritonClient, url, verbose, protocol);
 
     Triton::TritonModelInfo detectorModelInfo = Triton::setModel(modelName, serverAddress);
-    std::unique_ptr<DetectorInterface>  detector = createDetectorInstance(modelType, detectorModelInfo.input_w_, detectorModelInfo.input_h_);
+    std::unique_ptr<DetectorInterface> detector = createDetectorInstance(modelType, detectorModelInfo.input_w_, detectorModelInfo.input_h_);
     const auto coco_names = detector->readLabelNames(labelsFile);
 
-    std::vector<tc::InferInput*> inputs = { nullptr };
-    std::vector<const tc::InferRequestedOutput*> outputs = Triton::createInferRequestedOutput(detectorModelInfo.output_names_);
     tc::InferOptions options = Triton::createInferOptions(modelName, modelVersion);
 
-    cv::Mat frame;
-    std::vector<uint8_t> input_data;
-    cv::VideoCapture cap(videoName);
-
-#ifdef WRITE_FRAME
-    cv::VideoWriter outputVideo;
-    cv::Size S = cv::Size((int)cap.get(cv::CAP_PROP_FRAME_WIDTH),    // Acquire input size
-        (int)cap.get(cv::CAP_PROP_FRAME_HEIGHT));
-    int codec = cv::VideoWriter::fourcc('M', 'J', 'P', 'G');
-    outputVideo.open("processed.avi", codec, cap.get(cv::CAP_PROP_FPS), S, true);
-    if (!outputVideo.isOpened()) {
-        std::cout << "Could not open the output video for write: " << videoName << std::endl;
-        return -1;
-    }
-#endif
-
-    tc::Error err;
-    while (cap.read(frame)) {
+    if (sourceName.find(".jpg") != std::string::npos || sourceName.find(".png") != std::string::npos) 
+    {
+        // Inference on an image
+        cv::Mat image = cv::imread(sourceName);
+        if (image.empty()) {
+            std::cerr << "Could not open or read the image: " << sourceName << std::endl;
+            return -1;
+        }
         auto start = std::chrono::steady_clock::now();
-        input_data = detector->preprocess(frame, detectorModelInfo.input_format_, detectorModelInfo.type1_, detectorModelInfo.type3_,
-            detectorModelInfo.input_c_, cv::Size(detectorModelInfo.input_w_, detectorModelInfo.input_h_));
-
-        if (inputs[0] != nullptr) {
-            err = inputs[0]->Reset();
-            if (!err.IsOk()) {
-                std::cerr << "failed resetting input: " << err << std::endl;
-                exit(1);
-            }
-        }
-        else {
-            err = tc::InferInput::Create(
-                &inputs[0], detectorModelInfo.input_name_, detectorModelInfo.shape_, detectorModelInfo.input_datatype_);
-            if (!err.IsOk()) {
-                std::cerr << "unable to get input: " << err << std::endl;
-                exit(1);
-            }
-        }
-
-        err = inputs[0]->AppendRaw(input_data);
-        if (!err.IsOk()) {
-            std::cerr << "failed setting input: " << err << std::endl;
-            exit(1);
-        }
-
-        tc::InferResult* result;
-        std::unique_ptr<tc::InferResult> result_ptr;
-        if (protocol == Triton::ProtocolType::HTTP) {
-            err = tritonClient.httpClient->Infer(
-                &result, options, inputs, outputs);
-        }
-        else {
-            err = tritonClient.grpcClient->Infer(
-                &result, options, inputs, outputs);
-        }
-        if (!err.IsOk()) {
-            std::cerr << "failed sending synchronous infer request: " << err
-                << std::endl;
-            exit(1);
-        }
-
-        auto [infer_results, infer_shapes] = Triton::getInferResults(result, batch_size, detectorModelInfo.output_names_, detectorModelInfo.max_batch_size_ != 0);
-        result_ptr.reset(result);
+        std::vector<Yolo::Detection> detections = processSource(image, detector, options, tritonClient, detectorModelInfo, protocol, batch_size);
         auto end = std::chrono::steady_clock::now();
         auto diff = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
-        std::cout << "Infer time: " << diff << " ms" << std::endl;
-
-        std::vector<Yolo::Detection> detections = detector->postprocess(cv::Size(frame.cols, frame.rows),
-            infer_results, infer_shapes);
-
-#if defined(SHOW_FRAME) || defined(WRITE_FRAME)
-        double fps = 1000.0 / static_cast<double>(diff);
-        std::string fpsText = "FPS: " + std::to_string(fps);
-        cv::putText(frame, fpsText, cv::Point(10, 30), cv::FONT_HERSHEY_SIMPLEX, 1, cv::Scalar(0, 255, 0), 2);
-        for (auto&& detection : detections) {
-            cv::rectangle(frame, detection.bbox, cv::Scalar(255, 0, 0), 2);
-            cv::putText(frame, coco_names[detection.class_id],
-                cv::Point(detection.bbox.x, detection.bbox.y - 1), cv::FONT_HERSHEY_PLAIN, 1.2, cv::Scalar(0xFF, 0xFF, 0xFF), 2);
+        std::cout << "Infer time: " << diff << " ms" << std::endl;    
+        for (auto&& detection : detections) 
+        {
+            cv::rectangle(image, detection.bbox, cv::Scalar(255, 0, 0), 2);
+        }
+        cv::imwrite("processed_frame.png", image);
+    }
+    else 
+    {
+        // Inference on a video
+        cv::Mat frame;
+        cv::VideoCapture cap(sourceName);
+#ifdef WRITE_FRAME
+        cv::VideoWriter outputVideo;
+        cv::Size S = cv::Size((int)cap.get(cv::CAP_PROP_FRAME_WIDTH),    // Acquire input size
+            (int)cap.get(cv::CAP_PROP_FRAME_HEIGHT));
+        int codec = cv::VideoWriter::fourcc('M', 'J', 'P', 'G');
+        outputVideo.open("processed.avi", codec, cap.get(cv::CAP_PROP_FPS), S, true);
+        if (!outputVideo.isOpened()) {
+            std::cout << "Could not open the output video for write: " << sourceName << std::endl;
+            return -1;
         }
 #endif
+        while (cap.read(frame)) 
+        {
+            auto start = std::chrono::steady_clock::now();
+            std::vector<Yolo::Detection> detections = processSource(frame, detector, options, tritonClient, detectorModelInfo, protocol, batch_size);
+            auto end = std::chrono::steady_clock::now();
+            auto diff = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+            std::cout << "Infer time: " << diff << " ms" << std::endl;            
+
+#if defined(SHOW_FRAME) || defined(WRITE_FRAME)
+            double fps = 1000.0 / static_cast<double>(diff);
+            std::string fpsText = "FPS: " + std::to_string(fps);
+            cv::putText(frame, fpsText, cv::Point(10, 30), cv::FONT_HERSHEY_SIMPLEX, 1, cv::Scalar(0, 255, 0), 2);
+            for (auto&& detection : detections) {
+                cv::rectangle(frame, detection.bbox, cv::Scalar(255, 0, 0), 2);
+                cv::putText(frame, coco_names[detection.class_id],
+                    cv::Point(detection.bbox.x, detection.bbox.y - 1), cv::FONT_HERSHEY_PLAIN, 1.2, cv::Scalar(0xFF, 0xFF, 0xFF), 2);
+            }
+#endif
 #ifdef SHOW_FRAME
-        cv::imshow("video feed ", frame);
-        cv::waitKey(1);
+            cv::imshow("video feed ", frame);
+            cv::waitKey(1);
 #endif
 #ifdef WRITE_FRAME
-        outputVideo.write(frame);
+            outputVideo.write(frame);
 #endif
+        }
     }
 
     return 0;
