6a4872d53d5d26afe8ffc4a55ed9266945f26310
Francesco Oliva <olibartfast@gmail.com>
2024-10-21 09:32:16 +0200

    Update TensorflowClassifier.hpp

---
+++ src/tasks/classification/src/TensorflowClassifier.hpp
@@ -0,0 +1,18 @@
+#pragma once
+#include "TaskInterface.hpp"
+#include <algorithm>
+#include <numeric>
+#include <opencv2/opencv.hpp>
+#include <vector>
+#include <iostream>
+
+class TensorflowClassifier : public TaskInterface {
+public:
+    TensorflowClassifier(int input_width, int input_height, int channels);
+
+    std::vector<Result> postprocess(const cv::Size& frame_size, std::vector<std::vector<float>>& infer_results,
+                                    const std::vector<std::vector<int64_t>>& infer_shapes) override;
+
+    std::vector<uint8_t> preprocess(const cv::Mat& img, const std::string& format, int img_type1, int img_type3,
+                                    size_t img_channels, const cv::Size& img_size) override;
+};
