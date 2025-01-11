6cc9509747024fca4288485520eb87dccb0cb9d8
Francesco Oliva <olibartfast@gmail.com>
2025-01-11 21:26:48 +0100

    bash script updated

--- example_docker_client_run_optical_flow.sh
+++ example_docker_client_run_optical_flow.sh
@@ -1,9 +1,10 @@
 # same level of Dockerfile
 # docker build --rm -t computer-vision-triton-cpp-client .
+# video from https://www.pexels.com/it-it/video/montagne-natura-volando-spiaggia-5614887/
 docker run --rm \
 --network host \
 -v ${PWD}/data:/app/data computer-vision-triton-cpp-client \
-  --source=/app/data/12408393_3840_2160_24fps.mp4 \
+  --source=/app/data/5614887-hd_1280_720_25fps.mp4 \
   --model_type=raft \
   --model=raft_large_torchscript  \
   --serverAddress=localhost \
