b97d761927b9f9abfe0e82e3e4c8dfea1dd141e4
Francesco Oliva <olibartfast@gmail.com>
2025-01-11 20:40:32 +0100

    updated raft exporter sh

--- scripts/optical_flow/raft_exporter.sh
+++ scripts/optical_flow/raft_exporter.sh
@@ -10,6 +10,6 @@ docker run --rm -it --gpus=all \
 --ipc=host --ulimit memlock=-1 --ulimit stack=67108864 \
   -w /workspace \
   nvcr.io/nvidia/pytorch:24.12-py3 /bin/bash -cx \
-  "python raft_exporter.py --model-type large --output-dir /exports --device cuda --format traced"
+  "pip install onnx-graphsurgeon && python raft_exporter.py --model-type large --output-dir /exports --device cuda --format traced"
 
 echo "RAFT model ready."
