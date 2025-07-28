26491683eab862a3b1949d840561044f0de3c96c
Francesco Oliva <olibartfast@gmail.com>
2025-07-29 01:25:57 +0200

    Update run_tests_docker.sh

--- run_tests_docker.sh
+++ run_tests_docker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "Running unit tests in Docker container..."
-docker run --platform linux/amd64 --rm --entrypoint /bin/sh tritonic:latest -c "cd /app/build && ./tests/run_tests" 
+docker run --platform linux/amd64 --rm --entrypoint /bin/sh tritonic:dev -c "cd /app/build && ./tests/run_tests" 
